676b69614f77287f91502ed0850ac1c89d6e8676
jahnathan x <jahnathanexantus@gmail.com>
2022-04-13 10:36:10 -0400

    checked yelp api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,26 @@
-var apiKeyyelp = "av6vdy6Ds6E5OJz2sxNld0D7kKiuLBWiE99xQUXUp1QZ0IYEjRvV1-6KS29udQtCCZCnn328_-vI3fLJh9pZhlaXkqBxda7y8ZvOcj75WEaM42wx9cTbNbKvv5FVYnYx";
+var apiKeyyelp =
+	"av6vdy6Ds6E5OJz2sxNld0D7kKiuLBWiE99xQUXUp1QZ0IYEjRvV1-6KS29udQtCCZCnn328_-vI3fLJh9pZhlaXkqBxda7y8ZvOcj75WEaM42wx9cTbNbKvv5FVYnYx";
 
+function test() {
+	fetch(
+		"https://floating-headland-95050.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=%22chicken%22&location=%22orlando%22",
+		{
+			method: "GET",
+			headers: {
+				Authorization:
+					"Bearer av6vdy6Ds6E5OJz2sxNld0D7kKiuLBWiE99xQUXUp1QZ0IYEjRvV1-6KS29udQtCCZCnn328_-vI3fLJh9pZhlaXkqBxda7y8ZvOcj75WEaM42wx9cTbNbKvv5FVYnYx",
+			},
+		}
+	)
+		.then((response) => {
+		return response.json();
+    
+		}).then((data) =>{
+      console.log(data);
+    })
+		.catch((err) => {
+			console.error(err);
+		});
+}
 
-
-
-
-
-
-
-
-
-fetch("https://api.yelp.com/v3/businesses/search?term=%22chicken%22&location=%22orlando%22", {
-  "method": "GET",
-  "headers": {
-    "Authorization": "Bearer av6vdy6Ds6E5OJz2sxNld0D7kKiuLBWiE99xQUXUp1QZ0IYEjRvV1-6KS29udQtCCZCnn328_-vI3fLJh9pZhlaXkqBxda7y8ZvOcj75WEaM42wx9cTbNbKvv5FVYnYx"
-  }
-})
-.then(response => {
-  console.log(response);
-})
-.catch(err => {
-  console.error(err);
-});
+test();
