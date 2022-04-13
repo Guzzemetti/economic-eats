1c646c8e502bd4b43f3ee8c249ec283df6d46576
jahnathan x <jahnathanexantus@gmail.com>
2022-04-13 13:47:51 -0400

    checking buttom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
-var apiKeyyelp = 
-
+var apiKeyyelp =
 	"av6vdy6Ds6E5OJz2sxNld0D7kKiuLBWiE99xQUXUp1QZ0IYEjRvV1-6KS29udQtCCZCnn328_-vI3fLJh9pZhlaXkqBxda7y8ZvOcj75WEaM42wx9cTbNbKvv5FVYnYx";
+let zipCode = document.getElementsByClassName("mt-3");
+var apiLink = `https://floating-headland-95050.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=%22steak%22&location=%22${zipCode}%22`;
+var originScript = "https://floating-headland-95050.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=%22steak%22&location=%22$$orlando%22"
+
+
+
 
 function test() {
-	fetch(
-		"https://floating-headland-95050.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=%22chicken%22&location=%22orlando%22",
+	fetch(apiLink,
 		{
 			method: "GET",
 			headers: {
@@ -14,14 +18,20 @@ function test() {
 		}
 	)
 		.then((response) => {
-		return response.json();
-    
-		}).then((data) =>{
-      console.log(data);
-    })
+			return response.json();
+		})
+		.then((data) => {
+		console.log(data);
+		})
 		.catch((err) => {
 			console.error(err);
 		});
-}
+};
 
 test();
+
+function getResult(zipCode){
+	
+	console.log(getResult);
+}
+addEventListener("click",getResult())
