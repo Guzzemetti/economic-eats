caec94c9a7afc7ba64492af79037302cddfd9d74
John Hysong <johnahysong@gmail.com>
2022-04-13 22:20:03 -0400

    added js functionality to move from index to results page and bring the search input along with it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,24 @@
-var apiKeyyelp =
-	"av6vdy6Ds6E5OJz2sxNld0D7kKiuLBWiE99xQUXUp1QZ0IYEjRvV1-6KS29udQtCCZCnn328_-vI3fLJh9pZhlaXkqBxda7y8ZvOcj75WEaM42wx9cTbNbKvv5FVYnYx";
-// created search btn
-var searchBtn = document.getElementById("search-button");
-var zipCode = document.querySelector("#search-box")
-var originScript = "https://floating-headland-95050.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=%22$$orlando%22"
+// variable to identify the search button on index.html
+var searchButton = document.querySelector("#search-button")
 
-// place holder
+// function that checks that an input was made
+// if not, pop states that something needs to be entered
+// takes the input result and moves to the results page when the button is clicked
+function searchHandler(event) {
+	event.preventDefault();
 
-// test() takes the value from the user
-function test() {
-	 var textInput = zipCode.value.trim()
-	 if(textInput){
-		fetchData(textInput)
-	 }
-	 }
+	var searchText = document.querySelector("#search-text").value;
 
+	if (!searchText) {
+		console.error("Please put something in...");
+		alert("please enter a search")
+		return;
+	}
 
-// this is searching the data from api and cross-referencing input from user
-	 function fetchData(textInput){
-		 console.log(textInput);
-	fetch(`https://floating-headland-95050.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${textInput}`,
-		{
-			method: "GET",
-			headers: {
-				Authorization:
-					"Bearer av6vdy6Ds6E5OJz2sxNld0D7kKiuLBWiE99xQUXUp1QZ0IYEjRvV1-6KS29udQtCCZCnn328_-vI3fLJh9pZhlaXkqBxda7y8ZvOcj75WEaM42wx9cTbNbKvv5FVYnYx",
-			},
-		}
-	)
-		.then((response) => {
-			return response.json();
-		})
-		.then((data) => {
-		console.log(data);
-		})
-		.catch((err) => {
-			console.error(err);
-		});
-};
+	var queryString = './results.html?q=' + searchText;
 
+	location.assign(queryString);
+}
 
-
-searchBtn.addEventListener("click", test)
+// listens for button click. when clicked, function is run
+searchButton.addEventListener('click', searchHandler);
